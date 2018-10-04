6b09caeec154d8474b10a72d37f4eebcc1eadaa7
RJ.Hwang <rongjihuang@gmail.com>
2018-10-04 23:48:05 +0800

    Use lombok to simplify Entity definition
    Need to install lombok plugin in idea

--- src/main/java/tech/simter/beginner/jpa/po/Entity1.java
+++ src/main/java/tech/simter/beginner/jpa/po/Entity1.java
@@ -1,11 +1,14 @@
 package tech.simter.beginner.jpa.po;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
 /**
  * @author RJ
  */
 @Entity
+@Data
 public class Entity1 {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,28 +16,4 @@ public class Entity1 {
   @Column(length = 100, nullable = false, unique = true)
   private String name;
   private String describe;
-
-  public Integer getId() {
-    return id;
-  }
-
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getDescribe() {
-    return describe;
-  }
-
-  public void setDescribe(String describe) {
-    this.describe = describe;
-  }
 }
