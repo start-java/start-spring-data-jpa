304bc5df8a8bf281020ce4b7d1c68af75345b6c6
RJ.Hwang <rongjihuang@gmail.com>
2018-10-05 09:51:22 +0800

    Rename SampleTest to DDLTest

---
+++ src/test/java/tech/simter/beginner/jpa/DDLTest.java
@@ -0,0 +1,21 @@
+package tech.simter.beginner.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Debug for output the auto DDL (Data Definition Language) only.
+ *
+ * @author RJ
+ */
+@SpringJUnitConfig(UnitTestConfiguration.class)
+@DataJpaTest
+class DDLTest {
+  @Test
+  void test() {
+    assertEquals(1, 1);
+  }
+}
