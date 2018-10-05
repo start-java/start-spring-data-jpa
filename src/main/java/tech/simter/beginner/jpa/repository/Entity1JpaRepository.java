1c4ef34499d7c2dfcfffc5bac6b7e7ba2bb320b5
RJ.Hwang <rongjihuang@gmail.com>
2018-10-05 23:06:30 +0800

    Test use default interface method to get the max code

--- src/main/java/tech/simter/beginner/jpa/repository/Entity1JpaRepository.java
+++ src/main/java/tech/simter/beginner/jpa/repository/Entity1JpaRepository.java
@@ -1,10 +1,14 @@
 package tech.simter.beginner.jpa.repository;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import tech.simter.beginner.jpa.po.Entity1;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -46,4 +50,20 @@ public interface Entity1JpaRepository extends JpaRepository<Entity1, Integer> {
     nativeQuery = true
   )
   Optional<String> getMaxCodeOptional(String codePrefix);
+
+  /**
+   * Use {@link Pageable} to set the limit and order by.
+   *
+   * @return the query result or empty list if found nothing
+   */
+  @Query(value = "select code from Entity1 where code like ?1%")
+  List<String> findAllCodeStartingWith(String codePrefix, Pageable pageable);
+
+  /**
+   * Note: if entity exists and its code value is null, would be throw NPE.
+   */
+  default Optional<String> getMaxCode2(String codePrefix) {
+    return findAllCodeStartingWith(codePrefix, PageRequest.of(0, 1, Sort.by(Sort.Direction.DESC, "code")))
+      .stream().findFirst();
+  }
 }
